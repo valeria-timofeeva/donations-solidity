2cbb942ed6c6f4109cbd3e83eb2fd4be109e09c7
Valeria Timofeeva <veltimofeeva@gmail.com>
2022-02-27 16:42:02 +0400

    fixed script for deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,17 +3,17 @@ const { ethers } = require("hardhat");
 async function main() {
     const [deployer] = await ethers.getSigners();
 
-    console.log('Deploying contracts with the account:', deployer.address)
+    console.log('Deploying contracts with the account:', deployer.address);
 
-    const Donation = await ethers.getContractFactory('Donation')
+    const Donation = await ethers.getContractFactory('Donation');
     const donation = await Donation.deploy();
 
-    console.log('Token address:', donation.address)
-
-    main()
-        .then(() => process.exit(0))
-        .catch((error) => {
-            console.error(error);
-            process.exit(1);
-        });
+    console.log('Token address:', donation.address);
 }
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
